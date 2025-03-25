5533d49a4e688afdfaa17beb5537e7327c443f00
mvnu6 <mbengoemmanuel26@gmail.com>
2025-03-25 16:18:55 +0100

    test

--- src/test/java/com/example/devops/DevopsApplicationTests.java
+++ src/test/java/com/example/devops/DevopsApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.devops;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DevopsApplicationTests {
 
 	@Test
 	void contextLoads() {
+			Car car = new Car("AA11BB", "Ferrai", 2000);
+			String plateNumber = car.getPlateNumber();
+			assertEquals(plateNumber, "AA11BB");
 	}
 
 }
